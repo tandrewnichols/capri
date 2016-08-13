22d75e3aee85cf99cb5f01dc5fb569cc69500d82
Andrew Nichols <tandrewnichols@gmail.com>
2016-08-13 15:42:10 -0400

    Add a server

--- bin/capri.js
+++ bin/capri.js
@@ -1,1 +1,14 @@
 #!/usr/bin/env node
+
+var program = require('commander');
+var pkg = require('../package.json');
+
+program.version(pkg.version);
+
+program
+  .command('start')
+  .option('-p, --port <num>', 'Alternate port to which the application should listen')
+  .action((options) => {
+    var capri = require('../lib/capri');
+    capri.run(options);
+  });
