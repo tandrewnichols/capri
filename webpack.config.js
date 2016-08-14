1c5793310f85adf697f31498717e5249faa2358b
Andrew Nichols <tandrewnichols@gmail.com>
2016-08-14 19:31:10 -0400

    Get index.html serving

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry: './client/main.js',
+  output: {
+    path: __dirname,
+    publicPath: '/assets/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+};
