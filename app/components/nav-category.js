cd8786fb399ec9fd86e76968bfea90d8452927d4
Andrew Nichols <tandrewnichols@gmail.com>
2016-08-25 09:55:38 -0400

    Refactor drawer stylings so that category details don't slide left and right

--- app/components/nav-category.js
+++ app/components/nav-category.js
@@ -3,13 +3,13 @@ import React from 'react'
 class NavCategory extends React.Component {
   render() {
     return (
-      <div>
-        <div className={'drawer-item' + (this.props.last ? ' last' : '')} onClick={() => this.props.onExpandCategory(this.props.children)}>
+      <div className={'drawer-item' + (this.props.last ? ' last' : '')}>
+        <div className="drawer-heading" onClick={() => this.props.onExpandCategory(this.props.children)}>
           {this.props.children}
           <span className="pull-right badge">4</span>
-          <div className={'category-details' + (this.props.expand === this.props.children ? ' open' : '')}>
-            Foo
-          </div>
+        </div>
+        <div className={'category-details' + (this.props.expand === this.props.children ? ' open' : '')}>
+          Foo
         </div>
       </div>
     )
